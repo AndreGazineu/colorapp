2ae63ab61db5fe7e910d89c913f3041f9bbf1e18
Andrê Gazineu <109384816+AndreGazineu@users.noreply.github.com>
2025-07-10 11:46:07 -0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-// vite.config.js - VERSÃO CORRIGIDA PARA SUA ESTRUTURA
-
+// vite.config.js - VERSÃO FINAL E CORRETA
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // 1. Diz ao Vite que a raiz do seu código-fonte é a pasta 'public'
-  root: 'public',
-
-  // 2. Diz ao Vite para colocar os arquivos finais na pasta 'dist' na raiz do projeto
-  build: {
-    outDir: '../dist'
-  },
-
-  // 3. Diz ao Vite que o site final estará em um subdiretório
+  // A única configuração que precisamos é esta:
   base: '/colorapp/',
 })
